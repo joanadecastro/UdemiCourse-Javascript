20627d057a62c2d7f18ffa13d5b4fb68bb50b8bb
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-09 17:20:13 +0000

    variaveis e constantes

---
+++ fundamentos/comentario.js
@@ -0,0 +1,13 @@
+console.log('Linha 1')
+
+/*
+	comentario de
+	multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+*comentario de
+*multiplas linhas
+*/
+console.log('Linha 3')
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 30 //podemos redeclarar o var dentro do mm escopo
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 //a const é para ser usada c valores que nao vao mudar
+//c = 50    nao posso redeclarar uma cons
+
+console.log(c)
